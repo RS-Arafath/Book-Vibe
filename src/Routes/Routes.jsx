e239505e59a9dbf5b0b7078cd71dd3f07446842c
Arafath <rsarafath.signin@gmail.com>
2026-04-23 02:52:25 +0600

    fix: update component references to use 'element' syntax in Routes.jsx

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,35 +1,33 @@
-
 import { createBrowserRouter } from 'react-router';
-import { RouterProvider } from 'react-router';
-import MainLayOut from "../Layout/MainLayOut";
-import Books from "../Pages/Books/Books";
-import Homepage from "../Pages/HomePage/Homepage";
+import MainLayOut from '../Layout/MainLayOut';
+import Books from '../Pages/Books/Books';
+import Homepage from '../Pages/HomePage/Homepage';
 import ErrorPage from '../Pages/ErrorPage/ErrorPage';
 import SignIn from '../Pages/SignIn/SignIn';
 import SignUp from '../Pages/SignUp/SignUp';
 
-
 export const router = createBrowserRouter([
   {
     path: '/',
-    Component: MainLayOut,
+    element: <MainLayOut />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
-        Component: Homepage,
+        element: <Homepage />,
       },
       {
-        path: '/books',
-        Component: Books,
+        path: 'books',
+        element: <Books />,
       },
     ],
-    errorElement: <ErrorPage></ErrorPage>
-   
-  }, {
-    path: "signUp",
-    Component:SignIn
-  }, {
-    path: "signIn",
-    Component: SignUp
-  }
+  },
+  {
+    path: 'signUp',
+    element: <SignUp />,
+  },
+  {
+    path: 'signIn',
+    element: <SignIn />,
+  },
 ]);
